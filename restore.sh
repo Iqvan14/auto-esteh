cd0c35c77dec3c5e94b4346e0eb9ec90cb33aca8
Iqvanny Zubaidi <ivancool61@gmail.com>
2021-10-08 18:56:45 +0800

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -3,14 +3,6 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
-echo "Checking VPS"
-IZIN=$( curl http://akses.rpj08.my.id:81/akses | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
 fi
 echo "This Feature Can Only Be Used According To Vps Data With This Autoscript"
 echo "Please input link to your vps data backup file."
