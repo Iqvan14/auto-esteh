20175d3d41f66334720f38a95c3637377e6cc6fd
Iqvanny Zubaidi <ivancool61@gmail.com>
2021-10-16 15:21:35 +0800

    Update renew-ssr.sh

--- renew-ssr.sh
+++ renew-ssr.sh
@@ -3,7 +3,6 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
-fi
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/usr/local/shadowsocksr/akun.conf")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
